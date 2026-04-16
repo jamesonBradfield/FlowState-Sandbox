d58b9fc58668cf16aa17e9b57bd5217cd391d231
jameson <jamesonbradfield@gmail.com>
2026-04-16 14:44:07 -0600

    sorted scripts

--- Scripts/grab_focus.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _ready():
-	self.grab_focus.call_deferred()
--- Scripts/session_manager.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-## manages the 3d game session
-
-## NOTE: need to find out how we define host join architecture!
